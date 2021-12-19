93729a0ce854605ea6ddb3ab2c3a3a548a1b8779
Iddan Aaronsohn <iddan@flycode.com>
2021-12-19 15:40:48 +0200

    Fix tests

--- test/expected/error.js
+++ test/expected/error.js
@@ -5,10 +5,12 @@ module.exports = {
   code: 'W001',
   reason: 'Hardcoded <a> tag',
   evidence: /(Fork me on GitHub!)/,
+  text: 'Fork me on GitHub!',
   line: 13,
   scope: '      <a data-ga-category="github links" data-ga-label="fork" ' +
     'href="https://github.com/jwarby/jquery-awesome-cursor/fork" class="btn' +
     ' btn-lg btn-success"><i class="fa fa-code-fork"></i> Fork me on '+
     'GitHub!</a>',
-  character: 185
+  character: 185,
+  continued: true
 };
